fff9e99ffa0cff0605adb6f8623ef186f71f3919
Vineeth S <svineth.face@gmail.com>
2016-09-30 18:06:59 +0530

    Add files via upload

---
+++ Glue00P (1).user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Glue00P
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Disable copy pate restriction in skillrack
+// @author       Technophilic
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser.min.js
+// @require http://code.jquery.com/jquery-latest.js
+// @match *://www.skillrack.com/*
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+    /* jshint ignore:end */
+    /* jshint esnext: true */
+    //==============================================
+    var a=confirm("Click ok to Enable paste");
+    if(a)
+    {
+    alert('Will disable');
+    $('body').html($('body').html().replace('diff = nlen - olen;', 'diff = 0;'));
+    alert("Restriction Disabled");
+    $('#j_id_5d > span').click(function(){
+    $(this).text('Saved !!');
+    });
+    }
+    else
+    {
+    alert('restriction not disabled');
+    }
+    //==============================================
+    /* jshint ignore:start */
+]]></>).toString();
+                  var c = babel.transform(inline_src);
+eval(c.code);
+/* jshint ignore:end */
